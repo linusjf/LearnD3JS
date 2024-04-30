48856e920a80e6642e3efbd77b23a2becf69e92a
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-30 18:44:15 +0530

    feat: start on weather pages

--- Hello-Framework/src/data/openmeteo.json.js
+++ Hello-Framework/src/data/openmeteo.json.js
@@ -1,11 +1,19 @@
-import {
-  fetchWeatherApi
-} from "openmeteo";
+import { fetchWeatherApi } from "openmeteo";
 
 const params = {
   latitude: 19.0728,
   longitude: 72.8826,
-  hourly: "temperature_2m",
+  hourly: [
+    "temperature_2m",
+    "relative_humidity_2m",
+    "dew_point_2m",
+    "precipitation_probability",
+    "wind_speed_10m",
+    "wind_direction_10m",
+    "is_day"
+  ],
+  temperature_unit: "fahrenheit",
+  wind_speed_unit: "mph",
   timezone: "auto"
 };
 const url = "https://api.open-meteo.com/v1/forecast";
@@ -13,9 +21,12 @@ const responses = await fetchWeatherApi(url, params);
 
 // Helper function to form time ranges
 const range = (start, stop, step) =>
-  Array.from({
-    length: (stop - start) / step
-  }, (_, i) => start + i * step);
+  Array.from(
+    {
+      length: (stop - start) / step
+    },
+    (_, i) => start + i * step
+  );
 
 // Process first location. Add a for-loop for multiple locations or weather
 // models
@@ -40,7 +51,13 @@ const weatherData = {
       Number(hourly.timeEnd()),
       hourly.interval()
     ).map((t) => new Date((t + utcOffsetSeconds) * 1000)),
-    temperature2m: hourly.variables(0).valuesArray()
+    temperature2m: hourly.variables(0).valuesArray(),
+    relativeHumidity2m: hourly.variables(1).valuesArray(),
+    dewPoint2m: hourly.variables(2).valuesArray(),
+    precipitationProbability: hourly.variables(3).valuesArray(),
+    windSpeed10m: hourly.variables(4).valuesArray(),
+    windDirection10m: hourly.variables(5).valuesArray(),
+    isDay: hourly.variables(6).valuesArray()
   }
 };
 
