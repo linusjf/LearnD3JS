b80aeb0eb41ee2f007ee1140b909017ad15ec209
Linus Fernandes <linus.fernandes@gmail.com>
2024-05-02 15:42:27 +0530

    feat: add freecode examples

---
+++ FreeCodeCamp/changebarchart.js
@@ -0,0 +1,9 @@
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+d3.select("body")
+  .selectAll("div")
+  .data(dataset)
+  .enter()
+  .append("div")
+  .attr("class", "bar")
+  .style("height", (d) => d * 10 + "px");
