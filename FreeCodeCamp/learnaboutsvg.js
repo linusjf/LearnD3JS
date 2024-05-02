517d70b97484b89ac19cec067f795ac0ccf71e6c
Linus Fernandes <linus.fernandes@gmail.com>
2024-05-02 15:59:55 +0530

    fix: fix linter violations

--- FreeCodeCamp/learnaboutsvg.js
+++ FreeCodeCamp/learnaboutsvg.js
@@ -1,6 +1,6 @@
-const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+// const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 
 const w = 500;
 const h = 100;
 
-const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+d3.select("body").append("svg").attr("width", w).attr("height", h);
--- FreeCodeCamp/shapeswithsvg.js
+++ FreeCodeCamp/shapeswithsvg.js
@@ -1,10 +1,9 @@
-const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+// const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 
 const w = 500;
 const h = 100;
 
-const svg = d3
-  .select("body")
+d3.select("body")
   .append("svg")
   .attr("width", w)
   .attr("height", h)
