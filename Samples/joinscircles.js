c3ced052caf003052b3766d7aa00c04fe043c971
Linus Fernandes <linus.fernandes@gmail.com>
2024-05-07 11:46:25 +0530

    feat: update join example

--- Samples/joinscircles.js
+++ Samples/joinscircles.js
@@ -1,16 +1,36 @@
-var myData = [40, 10, 20, 60, 30];
+function getData() {
+  let data = [];
+  let numItems = Math.ceil(Math.random() * 5);
 
-d3.select(".chart")
-  .selectAll("circle")
-  .data(myData)
-  .join("circle")
-  .attr("cx", function(d, i) {
-    return i * 100 + 50;
-  })
-  .attr("cy", 50)
-  .attr("r", function(d) {
-    return 0.5 * d;
-  })
-  .style("fill", function(d) {
-    return d > 30 ? "orange" : "#eee";
-  });
+  for (let i = 0; i < numItems; i++) {
+    data.push(Math.random() * 60);
+  }
+
+  return data;
+}
+
+function update(data) {
+  d3.select(".chart")
+    .selectAll("circle")
+    .data(data)
+    .join("circle")
+    .attr("cx", function(d, i) {
+      return i * 100 + 50;
+    })
+    .attr("cy", 50)
+    .attr("r", function(d) {
+      return 0.5 * d;
+    })
+    .style("fill", function(d) {
+      return d > 30 ? "orange" : "#eee";
+    });
+}
+
+function updateAll() {
+  let myData = getData();
+  update(myData);
+}
+
+updateAll();
+
+d3.select("button").on("click", updateAll);
