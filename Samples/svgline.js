757fd90a9f07df2d2aae6da3c43c5318d8d0b235
Linus Fernandes <linus.fernandes@gmail.com>
2024-05-07 16:06:40 +0530

    feat: add svgshapes example

---
+++ Samples/svgline.js
@@ -0,0 +1,15 @@
+var width = 300;
+var height = 300;
+var svg = d3
+  .select(".svgcontainer")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+svg
+  .append("line")
+  .attr("x1", 100)
+  .attr("y1", 100)
+  .attr("x2", 200)
+  .attr("y2", 200)
+  .style("stroke", "rgb(255,0,255)")
+  .style("stroke-width", 5);
